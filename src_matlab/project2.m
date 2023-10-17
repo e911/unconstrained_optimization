2b2c7c1090d7a98f1cdce9396bce450d2c9accc0
Deepak Pandita <deepakpandita57@gmail.com>
2023-10-17 14:44:38 -0400

    prettify

--- src_matlab/project2.m
+++ src_matlab/project2.m
@@ -1,16 +1,18 @@
 % Project 2: Unconstrained Optimization
 
 %% for reproducibility initialize the random number generator
-% rng(20,"twister");
 rng(42,"twister");
 
 x = randn(100, 1);
+% x = 2*ones(100,1);
+
 max_iter=200;
 
 %% fun1
 [x_gd,hist_fx_gd,hist_fxfx1_gd]=gradientDescent(@fun1,@g_fun1,x,1e-5,max_iter);
 % disp(hist_fx_gd(end))
 fprintf("Optimum Value with GD: %f\n", hist_fx_gd(end))
+fprintf("Iterations: %d\n", length(hist_fx_gd)-1)
 
 figure
 plot(1:length(hist_fx_gd), hist_fx_gd);
@@ -22,6 +24,7 @@ saveas(gcf,'fun1_gd.jpg')
 [x_nm,hist_fx_nm,hist_fxfx1_nm]=newton(@fun1,@g_fun1,@h_fun1,x,1e-5,max_iter);
 % disp(hist_fx_nm(end))
 fprintf("Optimum Value with Newton: %f\n", hist_fx_nm(end))
+fprintf("Iterations: %d\n", length(hist_fx_nm)-1)
 
 figure
 plot(1:length(hist_fx_nm), hist_fx_nm);
@@ -33,6 +36,7 @@ saveas(gcf,'fun1_nm.jpg')
 [x_qn,hist_fx_qn,hist_fxfx1_qn]=quasiNewton(@fun1,@g_fun1,@h_fun1,x,1e-5,1000);
 % disp(hist_fx_qn(end))
 fprintf("Optimum Value with Quasi-Newton: %f\n", hist_fx_qn(end))
+fprintf("Iterations: %d\n", length(hist_fx_qn)-1)
 
 figure
 plot(1:length(hist_fx_qn), hist_fx_qn);
@@ -47,6 +51,7 @@ saveas(gcf,'fun1_qn.jpg')
 [x_gd,hist_fx_gd,hist_fxfx1_gd]=gradientDescent(@fun2,@g_fun2,x,1e-5,max_iter);
 % disp(hist_fx_gd(end))
 fprintf("Optimum Value with GD: %f\n", hist_fx_gd(end))
+fprintf("Iterations: %d\n", length(hist_fx_gd)-1)
 
 figure
 plot(1:length(hist_fx_gd), hist_fx_gd);
@@ -58,6 +63,7 @@ saveas(gcf,'fun2_gd.jpg')
 [x_nm,hist_fx_nm,hist_fxfx1_nm]=newton(@fun2,@g_fun2,@h_fun2,x,1e-5,max_iter);
 % disp(hist_fx_nm(end
 fprintf("Optimum Value with Newton: %f\n", hist_fx_nm(end))
+fprintf("Iterations: %d\n", length(hist_fx_nm)-1)
 
 figure
 plot(1:length(hist_fx_nm), hist_fx_nm);
@@ -69,6 +75,7 @@ saveas(gcf,'fun2_nm.jpg')
 [x_qn,hist_fx_qn,hist_fxfx1_qn]=quasiNewton(@fun2,@g_fun2,@h_fun2,x,1e-5,max_iter);
 % disp(hist_fx_qn(end))
 fprintf("Optimum Value with Quasi-Newton: %f\n", hist_fx_qn(end))
+fprintf("Iterations: %d\n", length(hist_fx_qn)-1)
 
 figure
 plot(1:length(hist_fx_qn), hist_fx_qn);
@@ -79,10 +86,12 @@ saveas(gcf,'fun2_qn.jpg')
 
 
 %% fun 3
-x = [20;20];
+% x = [20;20];
+x = randn(2,1);
 [x_gd,hist_fx_gd,hist_fxfx1_gd]=gradientDescent(@fun3,@g_fun3,x,1e-5,max_iter);
 % disp(hist_fx_gd(end))
 fprintf("Optimum Value with GD: %f\n", hist_fx_gd(end))
+fprintf("Iterations: %d\n", length(hist_fx_gd)-1)
 
 figure
 plot(1:length(hist_fx_gd), hist_fx_gd);
@@ -94,6 +103,7 @@ saveas(gcf,'fun3_gd.jpg')
 [x_nm,hist_fx_nm,hist_fxfx1_nm]=newton(@fun3,@g_fun3,@h_fun3,x,1e-5,max_iter);
 % disp(hist_fx_nm(end))
 fprintf("Optimum Value with Newton: %f\n", hist_fx_nm(end))
+fprintf("Iterations: %d\n", length(hist_fx_nm)-1)
 
 figure
 plot(1:length(hist_fx_nm), hist_fx_nm);
@@ -105,6 +115,7 @@ saveas(gcf,'fun3_nm.jpg')
 [x_qn,hist_fx_qn,hist_fxfx1_qn]=quasiNewton(@fun3,@g_fun3,@h_fun3,x,1e-5,max_iter);
 % disp(hist_fx_qn(end))
 fprintf("Optimum Value with Quasi-Newton: %f\n", hist_fx_qn(end))
+fprintf("Iterations: %d\n", length(hist_fx_qn)-1)
 
 figure
 plot(1:length(hist_fx_qn), hist_fx_qn);
@@ -113,23 +124,3 @@ ylabel("f(x)")
 title("Quasi-Newton Method")
 saveas(gcf,'fun3_qn.jpg')
 
-
-%% plotting
-% hist_fx = hist_fx_gd;
-% hist_fxfx1 = hist_fxfx1_gd;
-
-% figure
-% % subplot(1,2,1)
-% plot(1:length(hist_fx), hist_fx);
-% % plot(1:length(hist_fx_gd), hist_fx_gd, 'r', 1:length(hist_fx_nm), hist_fx_nm, 'g', 1:length(hist_fx_qn), hist_fx_qn, 'b');
-% xlabel("iterations")
-% ylabel("f(x)")
-% title("Convergence")
-% subplot(1,2,2)
-% plot(1:length(hist_fxfx1), hist_fxfx1);
-% % plot(1:length(hist_fxfx1), hist_fxfx1, 'r', 1:length(hist_fxfx1_nm), hist_fxfx1_nm, 'g', 1:length(hist_fxfx1_qn), hist_fxfx1_qn, 'b');
-% xlabel("iterations")
-% ylabel("f(x)-f(x-1)")
-% % title("Convergence")
-
-% saveas(gcf,'Convergence.jpg')
